c132899edb9935480d6344aeaad0514446cb46b0
Ahnabu <horairaabu013025@gmail.com>
2024-07-07 23:28:41 +0600

    environments updated and analytics included

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
-
-
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/StairTransition";
@@ -28,6 +28,8 @@ export default function RootLayout({ children }) {
         <StairTransition></StairTransition>
         <PageTransition>
           {children}
+          <Analytics />
+          <SpeedInsights/>
         </PageTransition>
        {/* <Footer/> */}
       </body>
